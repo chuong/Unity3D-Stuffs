15d92057f8840962b6ab326359a860411e73b5a3
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-04-10 22:04:56 +1000

    Add points and display score

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -11,8 +11,14 @@ public class GameController : MonoBehaviour {
 	public float startWait;
 	public float waveWait;
 
+	public GUIText scoreText;
+
+	private int score;
+
 	void Start()
 	{
+		score = 0;
+		UpdateScore ();
 		StartCoroutine(SpawnWaves ());
 	}
 
@@ -31,5 +37,17 @@ public class GameController : MonoBehaviour {
 			}
 			yield return new WaitForSeconds (waveWait);
 		}
+	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+		
+
+	void UpdateScore()
+	{
+		scoreText.text = "Score: " + score;
 	}
 }
